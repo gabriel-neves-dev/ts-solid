6b40dde44d3e3e578a6a9d56bae9d2b53364c800
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-05-08 15:52:01 -0300

    Classes de shopping cart separadas devidamente para cada uma ter apenas uma responsabilidade

---
+++ src/single-responsability/classes/shopping-cart.ts
@@ -0,0 +1,43 @@
+import { CartItem } from "./interfaces/cart-item";
+import { OrderStatus } from "./interfaces/order-status";
+
+
+
+
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = [];
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+
+  isEmpty(): boolean { // validação, mas é apenas uma para checar o carrinho, se encontra coesa
+    return this._items.length === 0;
+  }
+
+  
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+    this._items.length = 0;
+  }
+}
+
