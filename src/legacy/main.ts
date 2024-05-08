da7c7822c559f2da680a5712cd8d1c70d7a0a971
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-05-08 15:38:19 -0300

    Principios Solid, carrinho de compras legacy

---
+++ src/legacy/main.ts
@@ -0,0 +1,12 @@
+// import { ShoppingCartLegacy } from "./shopping-cart-legacy";
+
+// const shoppingCart = new ShoppingCartLegacy();
+// shoppingCart.addItem({ name: 'Camiseta', price: 49.91 });
+// shoppingCart.addItem({ name: 'Caderno', price: 9.9123 });
+// shoppingCart.addItem({ name: 'Lápis', price: 1.59 });
+
+// console.log(shoppingCart.items);
+// console.log(shoppingCart.total());
+// console.log(shoppingCart.orderStatus);
+// shoppingCart.checkout();
+// console.log(shoppingCart.orderStatus)
